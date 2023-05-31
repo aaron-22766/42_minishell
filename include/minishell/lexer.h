e1d7bf105823dfbde2bdc12bfda951717ac33677
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2023-05-31 16:06:30 +0200

    lexer header done

--- include/minishell/lexer.h
+++ include/minishell/lexer.h
@@ -15,17 +15,18 @@
 /*                                   ENUMS                                    */
 /* ************************************************************************** */
 
-enum e_token_type
+enum e_token_identifier
 {
-	INVALID,
-	COMMAND,
-	ARGUMENT,
-	I_RED,
-	O_RED,
-	HERE_DOC,
-	O_RED_A,
-	PIPE,
-	HD_EOF
+	WORD = 0b00000001,
+	COMMAND = 0b00000101,
+	FILE_NAME = 0b00001001,
+	HEREDOC_EOF = 0b00010001,
+	OPERATOR = 0b00000010,
+	I_RED = 0b00000110,
+	O_RED = 0b00001010,
+	HEREDOC = 0b00010010,
+	O_RED_A = 0b00100010,
+	PIPE = 0b01000010
 };
 
 /* ************************************************************************** */
@@ -34,7 +35,7 @@ enum e_token_type
 
 typedef struct s_tokens
 {
-	char			type;
+	char			id;
 	char			*content;
 	struct s_tokens	*next;
 }	t_tokens;
@@ -46,8 +47,19 @@ typedef struct s_tokens
 // lex.c
 t_tokens	*ft_lex(char *line);
 
+// split.c
+char		ft_split_at_operators(t_tokens *token);
+
+// evaluate.c
+void		ft_evaluate_tokens(t_tokens *tokens);
+
+// check.c
+char		*ft_check_syntax(t_tokens *tokens);
+
 // utils.c
-void		print_tokens(t_tokens *tokens);
+t_tokens	*ft_new_token(char *content);
 void		ft_free_tokens(t_tokens *tokens);
+size_t		ft_unquoted_char(char *str, const char *chars, const char *quotes);
+t_tokens	*print_tokens(t_tokens *tokens);
 
 #endif
