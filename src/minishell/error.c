c89d893e8f2c61273a3485fc5b69335f024501bd
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2023-06-01 17:00:44 +0200

    actually prints to stderr now

--- src/minishell/error.c
+++ src/minishell/error.c
@@ -1,16 +1,21 @@
 #include "../../include/minishell/minishell.h"
 
-void	ft_perror(int err, char *context)
+static void	ft_print_error(int err, char *context)
 {
-	ft_perror_exe("minishell", err, context);
+	if (err == ERR_MEM)
+		ft_eprintf("memory allocation failed while %s", context);
+	else if (err == ERR_TOKEN_SYNTAX)
+		ft_eprintf("syntax error near unexpected token `%s'", context);
 }
 
 void	ft_perror_exe(char *exe, int err, char *context)
 {
-	ft_printf("%s: ", exe);
-	if (err == ERR_MEM)
-		ft_printf("memory allocation failed while %s", context);
-	else if (err == ERR_TOKEN_SYNTAX)
-		ft_printf("syntax error near unexpected token `%s'", context);
-	ft_printf("\n");
+	ft_eprintf("%s: ", exe);
+	ft_print_error(err, context);
+	ft_eprintf("\n");
+}
+
+void	ft_perror(int err, char *context)
+{
+	ft_perror_exe("minishell", err, context);
 }
